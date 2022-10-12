c24453bf7565a55723ea04c0660ae623c6e313b1
Austin Velásquez <alejandra.canelp@gmail.com>
2022-10-11 18:08:28 -0600

    endpoint para autorizacion y rutas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->prefix('/auth')->group(function () {
+    Route::post('/login', 'loginUser')->name('login');;
+    Route::post('/register', 'createUser')->name('register');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    /* Route::controller(Department::class)->prefix('/department')->group(function () {
+        Route::get('/', 'index');
+        Route::get('/{id}', 'show');
+    }); */
 });
